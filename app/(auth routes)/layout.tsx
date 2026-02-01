079e419914b148e55ebf5ed4415a00e811afe88b
Baramykova Jane <baramikovajane@gmail.com>
2026-02-01 12:27:45 +0200

    delete api

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,17 +1,10 @@
-
 import { ReactNode } from "react";
-import { redirect } from "next/navigation";
-import { getMe } from "@/lib/api/serverApi";
 
-type Props = { children: ReactNode };
+type Props = {
+  children: ReactNode;
+};
 
-export default async function AuthLayout({ children }: Props) {
-  try {
-    const user = await getMe(); 
-    if (!user) redirect("/sign-in");
-  } catch {
-    redirect("/sign-in");
-  }
-  return <>{children}</>;
+export default function AuthLayout({ children }: Props) {
+ 
+  return <div>{children}</div>;
 }
-
